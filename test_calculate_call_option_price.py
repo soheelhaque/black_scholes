f85e40205ce730caeb444b227714dda64393ae7a
Soheel Haque-Everding <soheelhaque@hotmail.com>
2020-04-27 15:17:50 +0100

    Added unit tests (using pytest)

--- test_calculate_call_option_price.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from calculator import calculate_call_option_price
-
-
-class TestCalculate_call_option_price(TestCase):
-    def test_calculate_call_option_price(self):
-        price = calculate_call_option_price(strike_price=15, option_maturity_years=3.5, current_price=25,
-                                            volatility_percent=20, risk_free_rate_percent=5)
-        self.assertEqual(round(price, 3), 12.494)
---
+++ test_calculator.py
@@ -0,0 +1,29 @@
+# TODO: change name of imported function
+import json
+import unittest
+from calculator import calculate_call_option_price
+
+
+class TestOptionPriceCalculator(unittest.TestCase):
+    
+    def test_calculate_call_option_price_positive_ir(self):
+        # TODO: add parameter for call/put
+        model_inputs = '{"strike_price":105, "option_maturity_years":1, "current_price":100, ' \
+                       '"volatility_percent":20, "risk_free_rate_percent":5}'
+        json_in = json.loads(model_inputs)
+        expected_result = 8.021
+        actual_result = round(calculate_call_option_price(json_in),3)
+        self.assertEqual(expected_result, actual_result)
+
+    def test_calculate_call_option_price_negative_ir(self):
+        # TODO: add parameter for call/put
+        model_inputs = '{"strike_price":105, "option_maturity_years":1, "current_price":100, ' \
+                       '"volatility_percent":20, "risk_free_rate_percent":-5}'
+        json_in = json.loads(model_inputs)
+        expected_result = 4.185
+        actual_result = round(calculate_call_option_price(json_in),3)
+        self.assertEqual(expected_result, actual_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
